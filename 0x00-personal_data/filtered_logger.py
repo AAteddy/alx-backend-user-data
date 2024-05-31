89bc491a768f72831811d7c9ee96391ed783e002
Tedros Alem <tedsaasfaha@gmail.com>
2024-05-31 14:09:27 +0300

    [Log formatter] project-2 implementation

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -19,6 +19,7 @@ single regex.
 
 import re
 from typing import List
+import logging
 
 
 def filter_datum(
@@ -29,3 +30,20 @@ def filter_datum(
         regex = f"{field}=[^{separator}]*"
         message = re.sub(regex, f"{field}={redaction}", message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Return the log message obfuscated using Regex."""
+        org = super().format(record)
+        return filter_datum(self.fields, self.REDACTION, org, self.SEPARATOR)
