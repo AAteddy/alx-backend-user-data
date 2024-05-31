bd69228b546c0b47a8a7fac8b3b6f289d509964e
Tedros Alem <tedsaasfaha@gmail.com>
2024-05-31 13:53:44 +0300

    [fix error] requirement error

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -10,8 +10,8 @@ field will be obfuscated
 message: a string representing the log line
 separator: a string representing by which character
 is separating all fields in the log line (message)
-The function should use a regex to replace occurrences
-of certain field values.
+The function should use a regex to replace
+occurrences of certain field values.
 filter_datum should be less than 5 lines long and
 use re.sub to perform the substitution with a
 single regex.
@@ -24,7 +24,7 @@ from typing import List
 def filter_datum(
     fields: List[str], redaction: str, message: str, separator: str
 ) -> str:
-    """doc"""
+    """Returns the log message obfuscated using Regex."""
     for field in fields:
         regex = f"{field}=[^{separator}]*"
         message = re.sub(regex, f"{field}={redaction}", message)
