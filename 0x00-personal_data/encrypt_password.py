c676cf3d80b37984d890a6196c7f25b9d6ab0e50
Tedros Alem <tedsaasfaha@gmail.com>
2024-06-11 10:17:26 +0300

    [Check valid password] project-7 implemented

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python3
-"""User passwords should NEVER be stored in plain text in a database.
-
-Implement a hash_password function that expects one string
-argument name password and returns a salted, hashed password,
-which is a byte string.
-
-Use the bcrypt package to perform the hashing (with hashpw).
+"""Password Encryption & Validation Project Module.
+User passwords should NEVER be stored in plain text in a database.
 """
 import bcrypt
 
@@ -13,5 +8,35 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """A function that expects one string argument
     and returns a salted, hashed password.
+
+    Generates a salted and hashed password.
+
+    Args:
+            password (str): A string containing the plain text
+            password to be hashed.
+
+    Returns:
+            bytes: A byte string representing the salted, hashed password.
+
     """
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """A function that validate that the provided
+    password matches the hashed password.
+
+    Validates whether the provided password matches the hashed password.
+
+    Args:
+            hashed_password (bytes): A byte string representing
+            the salted, hashed password.
+            password (str): A string containing the plain text
+            password to be validated.
+
+    Returns:
+            bool: True if the provided password matches the hashed
+            password, False otherwise.
+
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
