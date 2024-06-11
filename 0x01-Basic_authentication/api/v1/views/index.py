c7a889bc5a880cb63096af5b0411c3cc16362e75
Tedros Alem <tedsaasfaha@gmail.com>
2024-06-11 20:35:32 +0300

    [Error handler: Forbidden] project forbidden

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -34,3 +34,12 @@ def unauthorized():
       - 401, unauthorized
     """
     abort(401)
+
+
+@app_views.route("/forbidden", methods=["GET"], strict_slashes=False)
+def forbidden():
+    """GET /api/v1/forbidden
+    Return:
+      - 403, forbidden
+    """
+    abort(403)
