d9c12d80f6d0936084da28e56eb8f06e413696c1
Tedros Alem <tedsaasfaha@gmail.com>
2024-06-11 10:08:32 +0300

    [Encrypting passwords] project-6 implemented

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -24,6 +24,23 @@ import os
 import mysql.connector
 
 
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Return the log message obfuscated using Regex."""
+        org = super().format(record)
+        return filter_datum(self.fields, self.REDACTION, org, self.SEPARATOR)
+
+
 def filter_datum(
     fields: List[str], redaction: str, message: str, separator: str
 ) -> str:
@@ -44,23 +61,6 @@ def filter_datum(
         regex = f"{field}=[^{separator}]*"
         message = re.sub(regex, f"{field}={redaction}", message)
     return message
-
-
-class RedactingFormatter(logging.Formatter):
-    """Redacting Formatter class"""
-
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
-
-    def __init__(self, fields: List[str]):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields
-
-    def format(self, record: logging.LogRecord) -> str:
-        """Return the log message obfuscated using Regex."""
-        org = super().format(record)
-        return filter_datum(self.fields, self.REDACTION, org, self.SEPARATOR)
 
 
 # PII fileds to be redacted
